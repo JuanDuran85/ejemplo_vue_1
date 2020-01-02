905ea74fda55e7cabbe08dc777acb21ce0d1f395
JuanDuran85 <duran85juan@gmail.com>
2020-01-02 18:16:24 -0300

    agregando axios

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,21 +1,43 @@
 <template>
-    <div>
-        <p>Hola desde Post.Vue</p>
-        <button class="btn btn-primary">Boton</button>
-    </div>
+  <div class="container">
+    <table class="table table-striped mt-4">
+      <thead>
+        <tr>
+          <th scope="col">Usuario Id</th>
+          <th scope="col">Id</th>
+          <th scope="col">Titulo</th>
+          <th scope="col">Post</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="post in posts" :key="post.id">
+          <th scope="row">{{post.id}}</th>
+          <td>{{post.userId}}</td>
+          <td>{{post.title}}</td>
+          <td>{{post.body}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    nombre: 'Ejemplo de Nombre',
-    data() {
-        return {
-            
-        }
-    },
-}
+  nombre: "Ejemplo de Nombre",
+  data() {
+    return {
+      posts: []
+    };
+  },
+    //antes de usar axios se debe instalar la libreria con npm o yarn
+  created() {
+      axios.get('http://jsonplaceholder.typicode.com/posts').then(posts=>{
+          this.posts = posts.data
+      });
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
