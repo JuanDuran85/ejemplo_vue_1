d44367ff3b8fdc38c140f251dde3d69ee5a11906
JuanDuran85 <duran85juan@gmail.com>
2020-01-02 16:36:38 -0300

    agregando bootstrap a componentes en vue

---
+++ src/components/Post.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <p>Hola desde Post.Vue</p>
+        <button class="btn btn-primary">Boton</button>
+    </div>
+</template>
+
+<script>
+export default {
+    nombre: 'Ejemplo de Nombre',
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
