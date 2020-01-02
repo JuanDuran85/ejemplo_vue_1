65a3b40ff844c548a6d7000a75352048ff315c3e
JuanDuran85 <duran85juan@gmail.com>
2020-01-02 18:39:04 -0300

    agregando router al proyecto

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import Posts from './components/Post.vue';
+//importamos la libreria del modulo de node
+import VueRouter from 'vue-router';
 
-Vue.config.productionTip = false
+//iniciamos el uso de la libreria
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    component: Posts
+  }
+];
+
+//instanciamos el nuevo rauter
+const router = new VueRouter({
+  routes: routes
+});
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  router
+}).$mount('#app');
